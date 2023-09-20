f17fcc3a82d1c089297b74ceaccf0330fec0221b
Андрей <98474058+aka-Steam@users.noreply.github.com>
2023-09-20 13:52:11 +0300

    added styles for form

--- src/views/LogInView.vue
+++ src/views/LogInView.vue
@@ -1,5 +1,5 @@
 <template>
-      <main>
+      <main class="main">
             <section class="section-authorization">
                   <form id="form-login" action="#" method="post" class="form">
                         <h1 class="form__title">Войдите в систему</h1>
@@ -23,11 +23,136 @@
                         </div>
                         <hr class="form__hr"/>
                         <div class="form__group form__group--center">
-                              <span class="form__text">Нет учетной записи?&nbsp;</span>
-                              <a id="open-registration-form-but" data-modal-id="#popupRegistr"
-                                    class="form__link open-registration-form-but">Регистрация</a>
+                              <span class="form__note">Нет учетной записи?&nbsp;</span>
+                              <RouterLink to="/signup" class="form__link open-registration-form-but">Регистрация</RouterLink>
                         </div>
                   </form>
             </section>
       </main>
 </template>
+
+<style scope>
+      .section-authorization {
+            display: flex;
+            justify-content: center;    
+      }
+
+      .form {
+            margin: 2% 5px 3rem 5px;
+            padding: 20px;
+            width: 40%;
+            max-width: 460px; 
+            min-width: 320px;
+            color: var(--color-text)
+      }
+
+      .form__group{
+            margin-bottom: 10px;
+      }
+
+      .form__group--center{
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+      }
+
+      .form__title{
+            margin: 0;
+            margin-bottom: 1rem;
+            text-align: center;
+            font-size: 2rem;
+            font-weight: normal;
+      }
+
+      .form__label{
+            display: inline-block;
+      }
+
+      .form__input{
+            padding: 0 10px;
+            width: 100%;
+            height: 2rem;
+            
+            -webkit-box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            box-sizing: border-box;
+
+            -webkit-appearance: none;
+            -moz-appearance: none;
+            -ms-appearance: none;
+            appearance: none;
+
+            outline: 0;
+            outline-offset: 0;
+
+            color: var(--color-accent);  
+            background-color: var(--color-background);
+            
+            border: 2px solid var(--color-text);
+            border-radius: 1.5rem;   
+      }
+      .form__input:focus {
+            border-color: var(--color-accent1);    
+      }
+
+      .form__link{
+            text-decoration: underline;
+      }
+
+      .form__link:link,
+      .form__link:visited{
+            color:var(--color-text);
+      }
+      .form__link:hover,
+      .form__link:active,
+      .form__link:focus{
+            color:var(--color-accent);
+            outline: 0;
+            outline-offset: 0;
+      }
+
+      .form__link--forget{
+            display: inline-block;
+            margin-bottom: 20px;
+      }
+
+      .form__button{
+            width: 100%; 
+            height: 2.5rem;
+
+            text-transform: uppercase;
+            font-weight: normal;
+            letter-spacing: 0.1em;
+
+            color: var(--color-text);
+            background-color: var(--color-background); 
+            border: 2px solid var(--color-text);
+            
+            border-radius: 2.5rem;
+      }
+      .form__button:hover,
+      .form__button:active{
+            cursor: pointer;
+            color: var(--color-accent);
+            border: 2px solid var(--color-accent);
+            outline: 0;
+            outline-offset: 0;      
+      }
+
+      .form__hr{
+            margin: 25px auto 15px auto;
+            height: 1px;
+            background-color: var(--color-text);
+            border:0;
+      }
+
+      .form__hr--bright{
+            background-color: var(--color-accent2);
+      }
+
+      .form__note{
+            display: inline-block;
+            line-height: normal;
+      }
+
+</style>
--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,9 +1,9 @@
 <template>
-      <main>
+      <main class="main">
             <section class="section-registration">
                   <form id="form-register" action="#" method="post" class="form">
                         <h1 class="form__title">Регистрация</h1>
-                        <hr class="form__hr form__hr--bright"/>
+                        <hr class="form__hr"/>
                         <div class="form__group">
                               <label for="register-name" class="form__label">Имя пользователя*</label>
                               <div class="form__input-container">
@@ -23,9 +23,9 @@
                               </div>
 
                               <ul class="pass-law-list">
-                                    <li class="passLawList__item">Ваш пароль должен содержать как минимум 8 символов.
+                                    <li class="pass-law-list__item">Ваш пароль должен содержать как минимум 8 символов.
                                     </li>
-                                    <li class="passLawList__item">Пароль не может состоять только из цифр.</li>
+                                    <li class="pass-law-list__item">Пароль не может состоять только из цифр.</li>
                               </ul>
                         </div>
                         <div class="form__group">
@@ -33,17 +33,35 @@
                               <div class="form__input-container">
                                     <input type="password" name="pass-repeat" id="register-pass-repeat" class="form__input">
                               </div>
-                              <span class="form__text fs08rem">Для подтверждения введите, пожалуйста, пароль ещё
+                              <span class="form__note fs08rem">Для подтверждения введите, пожалуйста, пароль ещё
                                     раз.</span>
                         </div>
                         <div class="form__group form__group--center mt20 mb5">
                               <button type="submit" class="form__button form__button--register">Зарегистрироваться</button>
                         </div>
-                        <span class="form__text fs08rem">Нажимая «Зарегистрироваться», Вы соглашаетесь с
-                              <a href="#" class="form__link">пользовательским соглашением</a> и
-                              <a href="#" class="form__link">политикой конфиденциальности</a>.
+                        <span class="form__note fs08rem">Нажимая «Зарегистрироваться», Вы соглашаетесь с
+                              <a href="#" class="form__link fs08rem">пользовательским соглашением</a> и
+                              <a href="#" class="form__link fs08rem">политикой конфиденциальности</a>.
                         </span>
                   </form>
             </section>
       </main>
 </template>
+
+<style scope>
+      .section-registration {
+            display: flex;
+            justify-content: center;    
+      }
+      .pass-law-list{
+            padding-left: 30px;
+            font-size: 0.8rem;
+      }
+
+      .form__button--register{    
+            border: 2px solid var(--color-accent2);
+      }
+      .fs08rem{    
+            font-size: 0.8rem;
+      }
+</style>
