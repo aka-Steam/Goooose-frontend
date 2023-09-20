541a4caa98f6d10acee2653d4d0a3813bfab76d9
Андрей <98474058+aka-Steam@users.noreply.github.com>
2023-09-21 00:37:17 +0300

    added map template

---
+++ src/components/Map.vue
@@ -0,0 +1,33 @@
+<script setup>
+ymaps.ready(init);
+    function init(){
+        // Создание карты.
+        var myMap = new ymaps.Map("map", {
+            // Координаты центра карты.
+            // Порядок по умолчанию: «широта, долгота».
+            // Чтобы не определять координаты центра карты вручную,
+            // воспользуйтесь инструментом Определение координат.
+            center: [55.76, 37.64],
+            // Уровень масштабирования. Допустимые значения:
+            // от 0 (весь мир) до 19.
+            zoom: 7
+        });
+    }
+</script>
+<template>
+      <section class="map-container">
+            <div id="map" class="map"></div>
+      </section>
+</template>
+<style scope>
+.map-container{
+      padding: 16px;
+}
+#map{
+      width:calc(clamp(320px, calc(100vw - 50px), 1920px));
+      height:calc(clamp(320px, calc(100vh - 32px), 800px));
+      border-radius: 20px;
+      overflow: hidden;
+}
+
+</style>
