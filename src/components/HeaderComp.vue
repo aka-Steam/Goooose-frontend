d4dad71c1dd5deb5f4b0da33114dabbdd68a3770
Андрей <98474058+aka-Steam@users.noreply.github.com>
2024-05-20 09:43:41 +0300

    The landing page has been updated

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -65,6 +65,7 @@ function burgerClick() {
       background-color: var(--color-background-header);
       box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.25);
       font-size: 20px;
+      z-index: 100;
 }
 
 .header__container {
