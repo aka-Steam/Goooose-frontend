93e42a9f3db432ddc13c18871141e6615785250c
Андрей <98474058+aka-Steam@users.noreply.github.com>
2024-06-14 13:25:38 +0300

    Fixed adding devices. Added an exit when the token becomes old

--- src/views/dashboardViews/MapView.vue
+++ src/views/dashboardViews/MapView.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { onMounted, onBeforeUnmount } from 'vue'
+import { useRouter } from 'vue-router'
 import { useStore } from 'vuex'
 import Menu from '../../components/SidebarMenu.vue'
 import DeviceMap from '../../components/Map.vue'
 import HeaderRoute from '../../components/HeaderRoute.vue'
 
+const router = useRouter()
 const store = useStore();
 
 let autoreloadIntervalID;
@@ -14,7 +16,18 @@ onMounted(() => {
         if (store.state.auth.status.loggedIn == false) {
             clearInterval(autoreloadIntervalID);
         };
-        store.dispatch('devicem/GET_ALL_DEVICES_FROM_API').then(console.log('Data arrived from Map page!'));
+        store.dispatch('devicem/GET_ALL_DEVICES_FROM_API').then(obj => {
+            if (obj["data"] !== undefined){
+                console.log('Data success arrived from Map page!')
+            }
+            // No data received. The obj is an error
+            else if (obj.response.status == 500 && obj.response.data.message == "Token has expired") {
+                console.log("LOGOUT. Token has expired")
+                store.dispatch('auth/logout');
+                localStorage.removeItem("user");// Костыль. Потому что не отрабатывает logout() из-за за устаревшего токена
+                router.push('/login');
+            }            
+        });
     }, 3000);
 })
 
